54d0eefa39eed357914821d8e184fba052d17931
Robert-Givens <rug1998@gmail.com>
2024-03-29 13:48:02 -0700

    Added new seasons, cleaned up dummy variables.

--- Code/data_analysis.py
+++ Code/data_analysis.py
@@ -11,9 +11,69 @@ from IPython.core.display import HTML
 finaldata = pd.read_csv("Data/Clean/final_data.csv")
 
 # Group by 'win' and calculate mean for 'out'
-tab = finaldata.groupby('win')['out'].mean().reset_index()
-sns.barplot(x='win', y='out', data=tab)
+tab = finaldata.groupby('ot')['win'].mean().reset_index()
+sns.barplot(x='ot', y='win', data=tab)
 plt.show()
+
+finaldata['de'].value_counts()
+
+finaldata["out"].describe()
+
+# Adjust bin size,outline on bars, match color theme for other plots
+# Label more clearly
+sns.distplot(finaldata["out"])
+plt.title('Distribution of "out"')
+plt.xlabel('"out" values')
+plt.ylabel('Frequency')
+plt.savefig()
+plt.show()
+
+
+
+# List of positions for which we want to generate the heatmap
+positions_list = ['rb', 'cb', 'fs', 'lb', 'dt', 'de', 'te', 'wr', 'p', 'ot',
+                  'og', 'ls', 'qb', 'k']
+
+# Initialize a matrix for average win rates
+win_rates = np.zeros((len(positions_list), int(finaldata[positions_list].max().max()) + 1))
+# Initialize a matrix for frequencies
+frequencies = np.zeros_like(win_rates)
+
+# Populate the matrices
+for i, position in enumerate(positions_list):
+    # Group by the number of injuries and calculate the average win rate
+    win_rate_group = finaldata.groupby(position)['win'].mean()
+    frequency_group = finaldata[position].value_counts()
+    
+    for num_injuries in win_rate_group.index:
+        win_rates[i, int(num_injuries)] = win_rate_group[num_injuries]
+        frequencies[i, int(num_injuries)] = frequency_group[num_injuries]
+
+# Define the annotations for the heatmap cells: "average win rate (frequency)"
+annotations = np.empty_like(win_rates, dtype=object)
+for i in range(win_rates.shape[0]):
+    for j in range(win_rates.shape[1]):
+        annotations[i, j] = f"{win_rates[i, j]:.2f} ({int(frequencies[i, j])})" if frequencies[i, j] > 0 else ""
+
+# Create the heatmap
+fig, ax = plt.subplots(figsize=(20, 10))
+sns.heatmap(win_rates, annot=annotations, fmt='', cmap='coolwarm', ax=ax,
+            cbar_kws={'label': 'Average Win Rate'}, xticklabels=range(win_rates.shape[1]),
+            yticklabels=positions_list)
+
+# Aesthetic elements
+# Creating the heatmap with combined annotations
+plt.figure(figsize=(16, 12))
+sns.heatmap(win_rates, annot=annotations, fmt='', cmap='coolwarm', cbar_kws={'label': 'Average Win Rate'})
+plt.title('Average Win Percentage / Frequency by Position and Number of Injuries', fontsize=16)
+plt.xlabel('Number of Injuries', fontsize=14)
+plt.ylabel('Position', fontsize=14)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.savefig("Output/Heat_Map.png")
+plt.show()
+# Save Heat Map plot
+
 
 # Linear models
 lm_model1 = smf.ols('win ~ out', data=finaldata).fit()
@@ -29,7 +89,14 @@ glm_model3 = smf.glm('win ~ out + age + lag_win_perc', data=finaldata, family=sm
 glm_model4 = smf.glm('win ~ out + C(team) + age + lag_win_perc', data=finaldata, family=sm.families.Binomial()).fit()
 glm_model5 = smf.glm('win ~ out + C(team) + age + win_perc + lag_win_perc', data=finaldata, family=sm.families.Binomial()).fit()
 
-
+smf.glm('win ~ wr * qb', data=finaldata, family=sm.families.Binomial()).fit().summary()
+# Loop through each position and fit the model
+for position in positions_list:
+    formula = f'win ~ {position}'
+    model = smf.glm(formula, data=finaldata, family=sm.families.Binomial()).fit()
+    print(f"Position: {position}")
+    print(model.summary())
+    print("\n")
 # View models
 dfoutput = summary_col([glm_model1, glm_model2, glm_model3, glm_model4, glm_model5], 
                        stars=True, 
--- Code/data_cleaning.py
+++ Code/data_cleaning.py
@@ -12,63 +12,49 @@ status_p['out'] = np.where(status_p['Active_Inactive'] == "Out", 1, 0)
 # Creating a checkpoint
 status = status_p.copy()
 
-# Calculate team-wide snap totals and max snaps by type
-status['Team_Snaps'] = status.groupby(['Team', 'Season', 'Week'])\
-    ['Total_Snaps'].transform('sum')
-status['max_o_snaps'] = status.groupby(['Team', 'Season', 'Week'])\
-    ['Offense_Snaps'].transform('max')
-status['max_d_snaps'] = status.groupby(['Team', 'Season', 'Week'])\
-    ['Defense_Snaps'].transform('max')
-status['max_s_snaps'] = status.groupby(['Team', 'Season', 'Week'])\
-    ['Special_Teams_Snaps'].transform('max')
-
-# Calculate total game snaps by summing the max offense, defense, and special teams snaps
-status['game_snaps'] = status['max_o_snaps'] + status['max_d_snaps'] + \
-    status['max_s_snaps']
-
-# Calculate snap rate as the proportion of total snaps to game snaps
-status['snap_rate'] = status['Total_Snaps'] / status['game_snaps']
-
 # Calculate the average snap rate for each player when not 'out'
-status['avg_snap_rate'] = status[status['out'] == 0].groupby(['gsis_id'])\
-    ['snap_rate'].transform('mean')
+avg_snaps = status_p[status_p['out'] == 0].groupby('gsis_id')['Total_Snaps']\
+    .mean().reset_index(name='avg_snaps')
 
-# Map the calculated average snap rates back to the original DataFrame
-avg_snap_rate_mapping = status.groupby('gsis_id')['avg_snap_rate']\
-    .first().to_dict()
-status['avg_snap_rate'] = status['gsis_id'].map(avg_snap_rate_mapping)
-
-# Remove duplicate records
-status = status.drop_duplicates()
+# Merge avg_snaps back to the original DataFrame on gsis_id
+status = pd.merge(status_p, avg_snaps, on='gsis_id', how='left')
 
 # Filter the DataFrame for 'out' players only for position dummies
 out_players = status[status['out'] == 1]
 
 # Mapping of positions to buckets
 position_to_bucket = {
-    'RB': 'RB', 'SS': 'DB', 'LB': 'LB', 'DB': 'DB', 'DT': 'DL',
-    'MLB': 'LB', 'FB': 'RB', 'DE': 'DL', 'TE': 'TE', 'CB': 'DB',
-    'WR': 'WR', 'P': 'P', 'OLB': 'LB', 'OT': 'OL', 'OG': 'OL',
-    'FS': 'DB', 'LS': 'LS', 'QB': 'QB', 'C': 'OL', 'ILB': 'LB',
-    'NT': 'DL', 'K': 'K', 'OL': 'OL', 'SAF': 'DB', 'G': 'OL',
-    'T': 'OL', 'DL': 'DL', 'PR': 'PR'
+    'RB': 'RB', 'SS': 'DB', 'LB': 'LB', 'DB': 'DB', 'DT': 'DT',
+    'MLB': 'LB', 'FB': 'RB', 'DE': 'DE', 'TE': 'TE', 'CB': 'CB',
+    'WR': 'WR', 'P': 'P', 'OLB': 'LB', 'OT': 'OT', 'OG': 'OG',
+    'FS': 'FS', 'LS': 'LS', 'QB': 'QB', 'C': 'C', 'ILB': 'LB',
+    'NT': 'DT', 'K': 'K', 'SAF': 'SS', 'G': 'OG',
+    'T': 'OT', 'DL': 'DL', 'PR': 'K'
 }
 
-# Apply the mapping to bucket the positions
+# Apply the mapping to categorize the positions
 out_players['position'] = out_players['position'].map(position_to_bucket)
 
 # Create dummy variables for different player positions
-positions = ['RB', 'DB', 'LB', 'DL', 'TE', 'WR', 'P', 'OL',
-             'LS', 'QB', 'K', 'PR']
-for position in positions:
-    out_players[position] = np.where(out_players['position'] == position, 1, 0)
+# This will create a new column for each position with 1s and 0s
+position_dummies = pd.get_dummies(out_players['position'])
+out_players = pd.concat([out_players, position_dummies], axis=1)
 
-# Perform aggregation on 'out_players' DataFrame
+# Create dummy variables for different player positions
+positions = ['RB', 'CB', 'FS', 'LB', 'DT', 'DE', 'TE', 'WR', 'P', 'OT',
+             'OG', 'LS', 'QB', 'K']
+
+# Perform aggregation on 'out_players' DataFrame without considering 'out' status
+# For each position, we use the max aggregation to indicate presence (1) or absence (0) of that position
 aggregated_data = out_players.groupby(['Team', 'Season', 'Week']).agg({
-    **{pos: 'sum' for pos in positions},  # Aggregate sum of position dummies for 'out' players
-    'out': 'sum',  # Count the total 'out' players
-    'avg_snap_rate': 'mean'  # Calculate the average snap rate for 'out' players
+    **{pos: 'max' for pos in positions},  # Use max to find if at least one player is present
+    'avg_snaps': 'mean',  # Calculate the average snap rate
+    'out':'sum'
 }).reset_index()
+
+# Convert boolean columns to integers
+position_columns = ['RB', 'CB', 'FS', 'LB', 'DT', 'DE', 'TE', 'WR', 'P', 'OT', 'OG', 'LS', 'QB', 'K']
+aggregated_data[position_columns] = aggregated_data[position_columns].astype(int)
 
 # Merge with the 'cap' DataFrame to include the age variable
 status = pd.merge(aggregated_data, cap[['Team', 'Season', 'AVG AGE']], 
@@ -108,7 +94,7 @@ finaldata = pd.merge(finaldata, team_season_wins, on=['Team', 'Season'], how='le
 finaldata.columns = [col.lower() for col in finaldata.columns]
 
 # Filter out 2015
-finaldata = finaldata[finaldata['season'] != 2015]
+finaldata = finaldata[finaldata['season'] != 2012]
 
 # Export to CSV
 finaldata.to_csv("Data/Clean/final_data.csv", index=False)
